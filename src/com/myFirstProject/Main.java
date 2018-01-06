c998c1cfe040e40b2f6a6d6b749da1d00c964575
David Bernthal <dbernthal@Davids-MacBook-Pro.local>
2018-01-05 19:52:49 -0800

    Hello Java Worldgit push -u origin master

---
+++ src/com/myFirstProject/Main.java
@@ -0,0 +1,10 @@
+package com.myFirstProject;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+        System.out.print("\nHello Java world!!!\n\n");
+    }
+
+}
